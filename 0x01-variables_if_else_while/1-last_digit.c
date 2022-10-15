b18c08a911841ed7cdb6ab49ae80112181a7afed
“sylvan452” <sylvan452@gmail.com>
2022-10-15 09:45:45 -0700

    first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void) /*main fuction*/
 
 	if (a > 5)
 	{
-		printf("last digit of %d is %d and is greater than 5\n", n, a);
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	}
 	else if (a == 0)
 	{
