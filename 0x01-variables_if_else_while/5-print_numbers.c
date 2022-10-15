263ee8dbf0994686ce2191318666b9afe4ebc46a
“sylvan452” <sylvan452@gmail.com>
2022-10-14 23:35:03 -0700

    fifth commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,11 +4,11 @@
  * main - print all single digit 0 to 9
  * Return: Always 0
 */
-#int main(void)
+int main(void)
 {
 	int num;
 
-	for num = 0; num < 10; num++)
+	for (num = 0; num < 10; num++)
 		printf("%d", num);
 
 	printf("\n");
