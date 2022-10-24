0c3bd3562efd82471759e7552680fe2b990da220
“sylvan452” <sylvan452@gmail.com>
2022-10-24 08:23:40 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - takes a pointer and update its value
+ *@n: int to check
+ *by sylva obah
+ *Return: 0 is success
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
