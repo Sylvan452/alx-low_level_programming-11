12752ec4b345ca889bd308e7e52b332b2cca162a
“sylvan452” <sylvan452@gmail.com>
2022-10-27 00:18:06 -0700

    task 3 updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  *@n2: second number
  *@r: buffer for result
  *@size_r: buffer size
- *
+ *ahhh! Crazy task! Expand your knowledge
  *Return: address of r or 0
  */
 
@@ -18,7 +18,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		;
 	for (j = 0; n2[j]; j++)
 		;
-	for (j = 0; n2[j]; j++)
+	if (i > size_r || j > size_r)
 		return (0);
 	m = 0;
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
