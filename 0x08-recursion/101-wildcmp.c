c23bf6d9b8b9496d6ddc12f21b5488dded8b401b
“sylvan452” <sylvan452@gmail.com>
2022-11-02 03:22:23 -0700

    final correction

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,10 +1,9 @@
 #include "main.h"
-#
 
 /**
  * str_checker - check if two strings are identical.
  * @s1: string_1 base address.
- * @s1: string_1 base address.
+ * @s2: string_2 base address.
  * @i: left index.
  * @j: special index. (joker)
  * Return: 1 if s is palindrome, 0 otherwise.
