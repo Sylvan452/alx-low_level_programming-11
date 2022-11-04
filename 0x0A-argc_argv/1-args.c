920eaf015b2492b7b6f873ff60dfac6c76300a98
“sylvan452” <sylvan452@gmail.com>
2022-11-04 04:15:04 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - start of program
+ * @argc: argument counter
+ * @argv: array pointer to the strings
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
