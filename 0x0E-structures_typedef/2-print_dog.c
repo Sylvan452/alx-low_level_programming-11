1a7c2966a0153aada3185cee6d9384173f12859f
“sylvan452” <sylvan452@gmail.com>
2022-11-14 09:15:53 -0800

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+
+/**
+ *print_dog - prints attribute inside struct
+ *@d: pointer to a struct
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	if (d->name == NULL)
+		d->name = "(nil)";
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
+}
