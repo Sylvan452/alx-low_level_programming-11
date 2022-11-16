5911197765b7068a25c0ec76013519ab94a4ab18
“sylvan452” <sylvan452@gmail.com>
2022-11-16 05:19:49 -0800

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,10 +17,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	if (size <= 0)
 		return (-1);
-	for (i = 1; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-			return (1);
+			return (i);
 	}
 	return (-1);
 }
