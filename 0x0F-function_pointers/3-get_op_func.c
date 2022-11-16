d9674c1f9c826c86b83b3a640a334e1b1c75876a
“sylvan452” <sylvan452@gmail.com>
2022-11-16 06:39:45 -0800

    adavance task 1

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,22 +10,25 @@
 
 int (*get_op_func(char *s))(int a, int b)
 {
-	int i;
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+		{ "+", op_add },
+		{ "-", op_sub },
+		{ "*", op_mul },
+		{ "/", op_div },
+		{ "%", op_mod },
+		{ NULL, NULL }
 	};
 
-	i = 0;
-	while (ops[i].op != NULL)
+	int i = 0;
+
+	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
+
 		i++;
 	}
-	return (NULL);
+
+	return (0);
+	
 }
